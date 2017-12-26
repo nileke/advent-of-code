e48905738fd09fb7739a10aa5645bb6af4c4026b
Nils Ekenbäck <ekenback@gmail.com>
2017-12-26 17:50:43 +0100

    Add: Solution day 6 second half

--- daysix/src/Debugger.java
+++ daysix/src/Debugger.java
@@ -6,6 +6,7 @@ import java.util.*;
 public class Debugger {
 
     private final int[] inputBank;
+    ArrayList<int[]> generatedCycles;
 
     public Debugger(String filepath) throws FileNotFoundException {
         File f = new File(filepath);
@@ -36,7 +37,7 @@ public class Debugger {
 
     public int debugInput() {
         Cycler cycler = new Cycler(inputBank);
-        ArrayList<int[]> generatedCycles = new ArrayList<>();
+        generatedCycles = new ArrayList<>();
         int[] lastCycle = cycler.cycleBank().clone();
         int count = 1; // First cycle
         while (!inList(generatedCycles, lastCycle)) {
@@ -47,8 +48,35 @@ public class Debugger {
         return count;
     }
 
+    public int findNumberOfCycles() {
+        Cycler cycler = new Cycler(inputBank);
+        generatedCycles = new ArrayList<>();
+        int[] lastCycle = cycler.cycleBank().clone();
+        int count = 0;
+        while (true) {
+            if (inList(generatedCycles, lastCycle)) {
+                int idx = findCycleStartIndex(generatedCycles, lastCycle);
+                return count - idx;
+            }
+            generatedCycles.add(lastCycle);
+            lastCycle = cycler.cycleBank().clone();
+            count++;
+        }
+        // return count;
+    }
+
+
     public static boolean inList(final List<int[]> list, final int[] candidate) {
         return list.parallelStream().anyMatch(a -> Arrays.equals(a, candidate));
+    }
+
+    public static int findCycleStartIndex(final List<int[]> list, final int[] candidate){
+        for(final int[] item : list){
+            if(Arrays.equals(item, candidate)){
+                return list.indexOf(item);
+            }
+        }
+        return -1;
     }
 
     public static void main(String[] args)  {
@@ -56,6 +84,7 @@ public class Debugger {
         try {
             Debugger debugger = new Debugger(filepath);
             System.out.println(debugger.debugInput());
+            System.out.println(debugger.findNumberOfCycles());
         } catch (FileNotFoundException e) {
             System.out.println("File not found, check filepath");
         }
--- daysix/src/DebuggerTest.java
+++ daysix/src/DebuggerTest.java
@@ -2,11 +2,18 @@ import junit.framework.TestCase;
 
 public class DebuggerTest extends TestCase {
 
-    public void testDebugger() {
-        int[] start = {0, 2, 7, 0};
+    private int[] start = {0, 2, 7, 0};
+
+    public void testDebugInput() {
         Debugger debug = new Debugger(start);
         int result = debug.debugInput();
         assertEquals(5, result);
+    }
+
+    public void testFindNumberOfCycles() {
+        Debugger debug = new Debugger(start);
+        int result = debug.findNumberOfCycles();
+        assertEquals(4, result);
     }
 
 }
