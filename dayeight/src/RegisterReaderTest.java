d2c61b0811907c395b415bf54d6367f49936e565
Nils Ekenbäck <ekenback@gmail.com>
2018-08-03 17:30:34 +0200

    Added testcases for single and multiple operations

--- dayeight/src/RegisterReaderTest.java
+++ dayeight/src/RegisterReaderTest.java
@@ -1,7 +1,39 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class RegisterReaderTest {
-    String command = "b inc 5 if a > 1";
+    RegisterReader register;
 
-    RegisterReader register = new RegisterReader();
+    String command = "b inc 5 if b < 1";
+
+    String[] testCommands = {
+                    "b inc 5 if a > 1",
+                    "a inc 1 if b < 5",
+                    "c dec -10 if a >= 1",
+                    "c inc -20 if c == 10"
+                    };
+
+    @BeforeEach
+    void setUp() {
+        register = new RegisterReader();
+    }
+
+    @Test
+    void testOneCommand() {
+        register.parseRegister(this.command);
+        Assertions.assertEquals(5, (int)register.register.get("b"));
+    }
+
+    @Test
+    void testMultipleCommands() {
+        for (String command : testCommands) {
+            register.parseRegister(command);
+        }
+        Assertions.assertEquals(1, (int)register.register.get("a"));
+        Assertions.assertEquals(0, (int)register.register.get("b"));
+        Assertions.assertEquals(-10, (int)register.register.get("c"));
+    }
 }
